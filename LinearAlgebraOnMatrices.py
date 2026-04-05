ae45225f361919617e56557f459ac417bcb3d4a1
Harish Divakar <harish.divakar.09@gmail.com>
2026-04-04 20:48:52 -0400

    Almost done. Need to finish readme, and clean the code up for functionality

--- LinearAlgebraOnMatrices.py
+++ LinearAlgebraOnMatrices.py
@@ -5,7 +5,7 @@ matrix = []
 print("Enter each entry for the 4x4 matrix")
 for i in range(4):
     while True:
-        row = input(f"Enter 4 values for row {i+1}, seperated by space: ").split()
+        row = input(f"Enter 4 values for row {i+1}, separated by space: ").split()
         if len(row)!=4:
             print("There must be exactly 4 values")
             continue
@@ -18,8 +18,9 @@ for i in range(4):
 
 A = sp.Matrix(matrix)
 
-print("\nMatrix entered: ")
+print("\nMatrix entered:  \n")
 sp.pprint(A) 
+
 
 #Declaring variables used for each task
 B = sp.Matrix([0,0])
@@ -28,76 +29,87 @@ steady_2 = None
 
 
 # Picking task to perform
-print("\nPick any task: \n1. An eigenvalue of 1 exists for the given matrix\n2. Finding steady state vector \n3. Revising matrix, then comparing steady state vectors \n4. Finding second largest eigenvalue")
 while True:
-   i = input("\nEnter 1, 2, 3, or 4: ")
-   if i not in {"1","2","3","4"}:
-       print ("Invalid input, try again")
-       continue
-   break
-
-"\n"
-# Task 1 - 
-if i == "1":
-    if 1 in A.eigenvals():
-        print ("Matrix A contains an eigenvalue of 0")
-    else:
-        print ("Matrix A does not contain an eigenvalue of 0")
-
-# Task 2
-if i == "2":
-    A = A.applyfunc(sp.nsimplify)
-    B = A-sp.eye(4)
-    S = B.nullspace()
-    if not S:
-        print ("\nThere is no steady state vector for this matrix")
-    else:
-        v=S[0]
-        steady_1 = v/sum(v)
-        print("\nSteady state vector is: ")
-        sp.pprint(steady_1)
+    print("\nPick any task: \n1. An eigenvalue of 1 exists for the given matrix\n2. Finding steady state vector \n3. Revising matrix, then comparing steady state vectors \n4. Finding second largest eigenvalue")
+    i = input("\nEnter 1, 2, 3, 4, or Q to exit: ")
+    if i.lower() == "q":
+        break
+    if i not in {"1","2","3","4"}:
+        print ("Invalid input, try again")
+        continue
     
-# Task 3 (not verfied yet)
-if i == "3":
-    while True:
-        i = input("Pick row number to change: ")
-        if i not in {"1","2","3","4"}:
-            print ("Invalid row, try again")
-            continue
-        break
-    while True:
-        j = input("Pick column number: ")
-        if j not in {"1","2","3","4"}:
-            print ("Invalid column, try again")
-            continue
-        break
-    while True:
-        k = input("Type the changed value for that row and column: ")
-        try:
-            k=float(k)
-            if k>=0:
-                break
-            else:
+    "\n"
+    # Task 1
+    if i == "1":
+        eigenval_list = list(A.eigenvals().keys())
+        rounded = [round(float(x),3) for x in eigenval_list]
+        if 1 in rounded:
+            print ("Matrix A contains an eigenvalue of 1")
+        else:
+            print ("Matrix A does not contain an eigenvalue of 1")
+
+    # Task 2
+    elif i == "2":
+        A = A.applyfunc(sp.nsimplify)
+        B = A-sp.eye(4)
+        S = B.nullspace()
+        if not S:
+            print ("\nThere is no steady state vector for this matrix")
+        else:
+            v=S[0]
+            steady_1 = v/sum(v)
+            print("\nSteady state vector is: ")
+            sp.pprint(steady_1)
+        
+    # Task 3
+    elif i == "3":
+        while True:
+            i = input("Pick row number to change: ")
+            if i not in {"1","2","3","4"}:
+                print ("Invalid row, try again")
+                continue
+            break
+        while True:
+            j = input("Pick column number: ")
+            if j not in {"1","2","3","4"}:
+                print ("Invalid column, try again")
+                continue
+            break
+        while True:
+            k = input("Type the changed value for that row and column: ")
+            try:
+                k=float(k)
+                if k>=0:
+                    break
+                else:
+                    print("Invalid input, try again")
+            except (ValueError):
                 print("Invalid input, try again")
-        except (ValueError):
-            print("Invalid input, try again")
-    row = int(i)-1
-    col = int(j)-1
-    val = float(k)
-    diff = val-A[row,col]
-    A[row,col] = val
-    if diff>=0:
-        A[col,col]-=diff
-    else:
-        A[col,col]+=diff
-    A = A.applyfunc(sp.nsimplify)
-    B = A-sp.eye(4)
-    S = B.nullspace()
-    if not S:
-        print ("\nThere is no steady state vector for this matrix")
-    else:
-        v=S[0]
-        steady_2 = v/sum(v)
-        print("\nNew steady state vector is: ")
-        sp.pprint(steady_2)
-#Task 4 
+        row = int(i)-1
+        col = int(j)-1
+        val = float(k)
+        diff = val-A[row,col]
+        C=A
+        C[row,col] = val
+        if diff>=0:
+            C[col,col]-=diff
+        else:
+            C[col,col]+=diff
+        C = C.applyfunc(sp.nsimplify)
+        B = C-sp.eye(4)
+        S = B.nullspace()
+        if not S:
+            print ("\nThere is no steady state vector for this matrix")
+        else:
+            v=S[0]
+            steady_2 = v/sum(v)
+            print("\nNew steady state vector is: ")
+            sp.pprint(steady_2)
+            
+    #Task 4 
+    elif i == "4":
+        eigenval_list = list(A.eigenvals().keys())
+        sorted_list = sorted(eigenval_list, reverse=True)
+        print("\nThe second largest eigenvalue is " + str(sorted_list[1]))
+        
+  
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # matrix-calculations-python
-This is the python code behind the urban revitalization project. This code represents the advantages of linear algebra, as it is programmable. This program is specifically suited for a 4x4 matrix that is column-stochastic, meaning that the sum of all elements in each column is 1. Also, the elements in the matrix cannot be negative and must be between 0 and 1. 
+About: This is the python implemenation of my linear algebra project that is dedicated towards evaluating an urban revitalization policy. It uses a transition matrix, a matrix that contains the probability of transitioning between different zones. With the use of python and the sympy library, evaluating these plans using matrices and linear algebra has becomes easier and faster. There are four tasks in this python implementation to give results that are useful in interpreting the efficacy of this plan.
+
+Constraints: For this python code to as intended, it must be a 4x4 matrix that is column stochastic (Every element in each column adds to 0), and contains of elements between 0 and 1. 
+
+Here is a quick overview of what each task does and how its results are interpreted
+
+Task 1 (Finding whether an eigenvaluie of 1 exists) - If an eigenvalue of 1 exists, the matrix can be considering an equlibrium population distribution, where there is a population distribution so that if the transition matrix is applied to the population distribution, it does not change the population distribution. This guarantees that the transition matrix will stabilize into a set amount of people within each zone, which is important to evaluate long term effects.
+
+Task 2 (Finding the steady state vector) - A steady state vector represents the population caused by a many, many transitions. The vector will provide the final population distribution, which is useful because it represents the expected change in each zone caused by the plan, so that the goal of revitalizing parts of the city may be achieved. 
+
+Task 3 (Sensitivity Analysis) - 
